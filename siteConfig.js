e08e2165e0d2001df0521e5f364e471444f5f8db
angelbeale <angel.beale@gmail.com>
2020-07-18 07:56:15 -0700

    first commit

---
+++ siteConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'ANGELBEALE',
+  shortName: 'LAM',
+  description: 'A test portfolio website',
+  url: 'https://gatsby-lam.vaporwavy.io', // Domain of your site without prefix!
+  prefix: '/',
+  author: 'angel beale',
+  twitter: ''
+}
